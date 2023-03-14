8f9ab25e2290684e92e18db791d2886969e26f49
solshuffle <solwicher@gmail.com>
2023-03-14 15:20:44 +0100

    change language from drawer

--- Components/Layout/Drawer.jsx
+++ Components/Layout/Drawer.jsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import MenuItem from "@mui/material/MenuItem";
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
   IconButton,
+  MenuItem,
   Typography,
 } from "@mui/material";
 import DehazeIcon from "@mui/icons-material/Dehaze";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import * as Styled from "./styles";
 
 export default function NavDrawer() {
@@ -22,7 +23,7 @@ export default function NavDrawer() {
     setOpen(false);
   }
 
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   function changeLanguage(e) {
     i18n.changeLanguage(e.target.value);
@@ -34,33 +35,23 @@ export default function NavDrawer() {
         <DehazeIcon fontSize="large" />
       </IconButton>
       <Styled.NavDrawer anchor={"right"} open={open} onClose={handleClose}>
-        <MenuItem>DEVICES</MenuItem>
-        <MenuItem>HOW IT WORKS</MenuItem>
-        <MenuItem>CONTACT</MenuItem>
+        <MenuItem>{t("devices")}</MenuItem>
+        <MenuItem>{t("how-it-works")}</MenuItem>
+        <MenuItem>{t("contact")}</MenuItem>
 
         <Accordion disableGutters elevation={0} square>
-          <AccordionSummary
-            expandIcon={<DehazeIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>Accordion 1</Typography>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>{t("language")}</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+            <MenuItem component="button" onClick={changeLanguage} value="en">
+              {t("english")}
+            </MenuItem>
+            <MenuItem component="button" onClick={changeLanguage} value="es">
+              {t("spanish")}
+            </MenuItem>
           </AccordionDetails>
         </Accordion>
-
-        <button onClick={changeLanguage} value="en">
-          English
-        </button>
-        <button onClick={changeLanguage} value="es">
-          Español
-        </button>
       </Styled.NavDrawer>
     </>
   );
